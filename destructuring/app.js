3b35e4df78b78f97a561f0bcc213503e4b475234
ADzhyhan <a.dzhyhan@gmail.com>
2021-08-12 01:13:54 +0300

    add solving for third and fourth tasks

--- destructuring/app.js
+++ destructuring/app.js
@@ -77,20 +77,68 @@
 
 
 //2
-const organisation = {
-  name: 'Google',
-  info: { 
-    employees: ['Vlad', 'Olga'], 
-    partners: ['Microsoft', 'Facebook', 'Xing'], 
-  }
+// const organisation = {
+//   name: 'Google',
+//   info: { 
+//     employees: ['Vlad', 'Olga'], 
+//     partners: ['Microsoft', 'Facebook', 'Xing'], 
+//   }
+// }; 
+
+// function getInfo({
+//   name = 'Unknown,',
+//   info: { partners: [first = 'none', second = 'none'] = [] } = {}
+// } = {}) {
+//   console.log(`Name: ${name}`);
+//   console.log(`${first}, ${second}`);
+// } 
+
+// getInfo(organisation); 
+
+//3
+let user = {
+  "guid": "dd969d30-841d-436e-9550-3b0c649e4d34",
+  "isActive": false,
+  "balance": "$2,474.46",
+  "age": 30,
+  "eyeColor": "blue",
+  "name": "Tameka Maxwell",
+  "gender": "female",
+  "company": "ENOMEN",
+  "email": "tamekamaxwell@enomen.com",
+  "phone": "+1 (902) 557-3898",
+  "tags": [
+    "aliquip",
+    "anim",
+    "exercitation",
+    "non",
+  ],
+  "friends": [
+    {
+      "id": 0,
+      "name": "Barber Hicks"
+    },
+    {
+      "id": 1,
+      "name": "Santana Cruz"
+    },
+    {
+      "id": 2,
+      "name": "Leola Cabrera"
+    }
+  ],
 }; 
 
-function getInfo({
-  name = 'Unknown,',
-  info: { partners: [first = 'none', second = 'none'] = [] } = {}
-} = {}) {
-  console.log(`Name: ${name}`);
-  console.log(`${first}, ${second}`);
-} 
+const { 
+  name = '', 
+  balance = '', 
+  email = '', 
+  tags: [ first = '', , , last = '' ] = [],
+  friends: [{ name: nameFriend = '' }] = [],
+} = user;  
 
-getInfo(organisation);
+console.log(name, balance, email, first, last, nameFriend); 
+
+//4 
+const newUser = [ ...user.tags, ...user.friends ];
+console.log(newUser);
