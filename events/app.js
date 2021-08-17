6941e7e8acf673d75fdbe93a14e958804922439b
ADzhyhan <a.dzhyhan@gmail.com>
2021-08-17 13:00:24 +0300

    add structure of files and task for homework #2

--- events/app.js
+++ events/app.js
@@ -47,12 +47,20 @@
 
 
 //4
-const btn = document.getElementById('btn-generate');
-const ul = document.querySelector('ul'); 
+// const btn = document.getElementById('btn-generate');
+// const ul = document.querySelector('ul'); 
 
-btn.addEventListener('click', (e) => {
-  const number = ul.children.length + 1; 
-  const li = document.createElement('li'); 
-  li.textContent = `Item ${number}`;
-  ul.appendChild(li);
-})
+// btn.addEventListener('click', (e) => {
+//   const number = ul.children.length + 1; 
+//   const li = document.createElement('li'); 
+//   li.textContent = `Item ${number}`;
+//   ul.appendChild(li);
+// }) 
+
+
+
+//--Homework #2-- 
+
+//Tasks 
+
+//1 Реализовать примитивный дропдаун. Изначально все dropdown-menu скрыты через класс .d-none. При клике на dropdown-item должен отображаться блок dropdown-menu который вложен именно в тот dropdown-item на котором произошел клик. При повторном клике на этот же dropdown-item блок dropdown-menu должен закрыться. При клике на любой другой dropdown-item уже открытый dropdown-menu должен закрываться а на тот который кликнули открываться. 
